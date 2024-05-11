d5fb3a9195c97f88996e12633d3125a47a2746a3
David-Avila <aviladurandavid613@gmail.com>
2024-05-11 15:03:58 -0400

    Added Linked In profile

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function GoToGitHub(){
     window.open("https://github.com/David-Avila", "_blank");
 }
+
+function GoToLinkedIn(){
+    window.open("www.linkedin.com/in/david-avila-duran-12568a222", "_blank");
+}
