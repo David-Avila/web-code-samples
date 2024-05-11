f1de57fc745c6615771937adca28c1fd0dbdc0f3
David-Avila <aviladurandavid613@gmail.com>
2024-05-11 15:22:14 -0400

    Animate cards at the begining

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+const samples = document.getElementsByClassName("SampleDiv");
+
+for(let i = 0; i < samples.length; i++){
+    window.setTimeout(function(){
+        samples[i].classList.add("show");
+    }, 200 * i);
+}
+
+
+
+
+
+
 function GoToGitHub(){
     window.open("https://github.com/David-Avila", "_blank");
 }
